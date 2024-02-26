aa0f9704cd769df5541e61891f231b7961e89b94
Leslie747401 <leslie747401@gmail.com>
2024-02-27 00:15:25 +0530

    changed loading.tsx

--- app/(main)/loading.tsx
+++ app/(main)/loading.tsx
@@ -5,7 +5,7 @@ import React from 'react'
 export default function Loading() {
   return (
     <div className='w-full h-screen flex justify-center items-center'>
-      Loading...
+      <Loader/>
     </div>
   )
 }
