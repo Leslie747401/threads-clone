0911de6c467cdd1bb78cba1bfddc72c25531b08e
Leslie747401 <leslie747401@gmail.com>
2024-02-29 14:26:02 +0530

    Added Fucntional UI to CreateDrawer and CreateDialog

--- components/CreateDialog.tsx
+++ components/CreateDialog.tsx
@@ -12,12 +12,17 @@ import {
   DialogTrigger,
 } from "@/components/ui/dialog"
 import { X } from "lucide-react"
+import Skeleton from "react-loading-skeleton"
+import Loader from "./Loader"
 
 export function CreateDialog() {
 
   const [userThread, setUserThread] = useState<string>('');
   const textAreaRef = useRef<HTMLTextAreaElement | null>(null);
   const [postImage,setPostImage] = useState<string>();
+  const [imageLoading,setImageLoading] = useState(false);
+  const [loading,setLoading] = useState(false);
+  const [openDialog,setOpenDialog] = useState(false);
 
   // It is used to resize (go to new line) the textarea field when it exceeds the width of textarea or when we press enter. 
   useEffect(() => {
@@ -27,8 +32,22 @@ export function CreateDialog() {
     }
   },[userThread]);
 
+  async function handleShare(){
+    setLoading(true);
+
+    setTimeout(()=>{                // Fake Delay
+        setLoading(false);
+        setOpenDialog(false);
+    },3000);
+  }
+
+  async function handleCancel(){
+    setUserThread(''); 
+    setPostImage('')
+  }
+
   return (
-    <Dialog>
+    <Dialog open={openDialog} onOpenChange={setOpenDialog}>
 
       <DialogTrigger asChild>
 
@@ -54,23 +73,35 @@ export function CreateDialog() {
                 />
                 
                 <p className={`text-lg text-[#a9a9a9] hover:text-black dark:text-[#a9a9a9] dark:hover:text-white transition-all`}>Create</p>
+
             </div>
             
         </div>
 
       </DialogTrigger>
 
-      <DialogContent className="w-full max-sm:h-screen sm:max-w-[575px] z-50" onInteractOutside={(e) => {setUserThread(''); setPostImage('')}}>
+      <DialogContent className="w-full max-sm:h-screen sm:max-w-[575px] z-50" onInteractOutside={(e) => {
+        
+        if(loading || imageLoading){
+          e.preventDefault();
+        }
+      
+        else{
+          setUserThread(''); 
+          setPostImage('');
+        }
+        
+        }}>
 
-        <div className="flex justify-between p-6 px-8">
+        <div className="flex justify-between p-5 px-8 items-center">
           
-          <DialogClose asChild onClick={() => {setUserThread(''); setPostImage('')}}>
-            <p className="cursor-pointer">Cancel</p>
+          <DialogClose asChild onClick={handleCancel}>
+            <p className={`${imageLoading || loading ? 'cursor-pointer h-[28px] w-[52px] flex justify-center pointer-events-none' : 'cursor-pointer h-[28px] w-[52px] flex justify-center'}`}>{loading ? <Loader/> : 'Cancel'}</p>
           </DialogClose>
           
-          <p className="font-medium">New Thread</p>
+          <p className="font-medium h-[28px]">New Thread</p>
           
-          <button className="text-blue-500">Share</button>
+          <button className={`${imageLoading ? 'text-blue-500 h-[28px] w-[52px] flex justify-center pointer-events-none' : 'text-blue-500 h-[28px] w-[52px] flex justify-center'}`} onClick={handleShare}>{loading ? <Loader/> : 'Share'}</button>
         
         </div>
 
@@ -93,62 +124,81 @@ export function CreateDialog() {
             
             <textarea placeholder="Start a thread..." rows={1} className="bg-white dark:bg-[#171717] outline-none resize-none overflow-hidden mb-2 placeholder:text-[#afafaf]  dark:placeholder:text-[#7a7a7a]" value={userThread} onChange={(e) => setUserThread(e.target.value)} ref={textAreaRef} required/>
 
-            { postImage ? 
-
-                <div className="w-fit relative border border-[#d1d1d1] rounded-xl">
-                  <img
-                    src={postImage}
-                    alt="post-image"
-                    className="h-[300px] w-full rounded-xl"
-                  />
-                  <div className="absolute top-4 right-3 p-1 cursor-pointer bg-[#6b6b6b] bg-opacity-60 rounded-full" onClick={() => setPostImage('')}>
-                    <X width={18} height={18} className="text-white"/>
-                  </div>
-              </div> 
+            { 
             
-            : 
+              imageLoading || postImage ? 
 
-              <UploadButton endpoint='imageUploader' className="max-w-fit"
+                ( postImage ?
 
-                onClientUploadComplete={(res) => {
-                  // Do something with the response
-                  console.log("Files: ", res);
-                  setPostImage(res[0].url);
-                }}
-                onUploadError={(error: Error) => {
-                  // Do something with the error.
-                  alert(`ERROR! ${error.message}`);
-                }}
-
-                appearance={{
-                  allowedContent : 'hidden',
-                  button : 'h-fit w-fit bg-white dark:bg-[#171717]'
-                }}
+                  <div className="w-fit relative border border-[#d1d1d1] dark:!border-[#393939] rounded-xl">
+                    
+                    <img
+                      src={postImage}
+                      alt="post-image"
+                      className="h-[300px] w-full rounded-xl"
+                    />
+                    
+                    <div className="absolute top-4 right-3 p-1 cursor-pointer bg-[#6b6b6b] bg-opacity-60 rounded-full" onClick={() => setPostImage('')}>
+                      <X width={18} height={18} className="text-white"/>
+                    </div>
+                  
+                  </div> 
                 
-                content={{      
-                  button({ready}){
-                    if(ready)
-                    return <>
-                      <Image
-                        src='/assets/images/white-gallery.png'
-                        width={18}
-                        height={18}
-                        alt="link-icon"
-                        className="dark:hidden cursor-pointer"
-                      />
+                  :
 
-                      <Image
-                        src='/assets/images/black-gallery.png'
-                        width={18}
-                        height={18}
-                        alt="link-icon"
-                        className="hidden dark:block cursor-pointer"
-                      />                
-                    </>
-                  },
-                }}
-              />
-            
+                  <Skeleton width={300} height={300}/>
+
+                )
+
+
+                : 
+
+                <UploadButton endpoint='imageUploader' className="max-w-fit"
+
+                  onUploadProgress={() => {
+                      setImageLoading(true);
+                    }}
+
+                  onClientUploadComplete={(res) => {
+                    // Do something with the response
+                    console.log("Files: ", res);
+                    setImageLoading(false)
+                    setPostImage(res[0].url);
+                  }}
+                  onUploadError={(error: Error) => {
+                    // Do something with the error.
+                    alert(`ERROR! ${error.message}`);
+                  }}
+
+                  appearance={{
+                    allowedContent : 'hidden',
+                    button : 'h-fit w-fit bg-white dark:bg-[#171717]'
+                  }}
+
+                  content={{      
+                    button({ready}){
+                      if(ready)
+                      return <>
+                        <Image
+                          src='/assets/images/white-gallery.png'
+                          width={18}
+                          height={18}
+                          alt="link-icon"
+                          className="dark:hidden cursor-pointer"
+                        />
+
+                        <Image
+                          src='/assets/images/black-gallery.png'
+                          width={18}
+                          height={18}
+                          alt="link-icon"
+                          className="hidden dark:block cursor-pointer"
+                        />                
+                      </>
+                    },
+                  }}
+                />
+
             }
 
 
--- components/CreateDrawer.tsx
+++ components/CreateDrawer.tsx
@@ -1,152 +1,48 @@
-// import * as React from "react"
-// import Image from "next/image"
-// import { useState , useEffect , useRef } from "react"
-
-// import { Button } from "@/components/ui/button"
-// import {
-//   Drawer,
-//   DrawerClose,
-//   DrawerContent,
-//   DrawerDescription,
-//   DrawerFooter,
-//   DrawerHeader,
-//   DrawerTitle,
-//   DrawerTrigger,
-// } from "@/components/ui/drawer"
-// import { ArrowLeft } from "lucide-react"
-
-// export function DrawerDemo() {
-
-//   const [userThread, setUserThread] = useState<string>('');
-//   const textAreaRef = useRef<HTMLTextAreaElement | null>(null);
-
-//   useEffect(() => {
-//     if (textAreaRef.current) {
-//       textAreaRef.current.style.height = 'auto';
-//       textAreaRef.current.style.height = textAreaRef.current.scrollHeight + 'px';
-//     }
-//   },[userThread]);  
-
-//   return (
-//     // dismissible = false is used to disable dragging of the drawer
-//     <Drawer dismissible={false}>
-
-//       <DrawerTrigger asChild>
-
-//         <div className="block sm:hidden">
-            
-//             {/* Create-icon for dark Mode */}
-//             <Image
-//                 src='/assets/images/grey-create.png'
-//                 width={24}
-//                 height={24}
-//                 alt="logo"
-//                 className="hidden dark:block"
-//             />
-
-//             {/* Create-icon for White Mode */}
-//                 <Image
-//                 src='/assets/images/grey-create.png'
-//                 width={24}
-//                 height={24}
-//                 alt="logo"
-//                 className="dark:hidden"
-//             />
-        
-//         </div>
-            
-//       </DrawerTrigger>
-
-//       <DrawerContent className="w-full h-screen">
-
-//         <div className="flex justify-between p-6">
-            
-//             {/* OnClick function is used to erase the input provided by the user */}
-//             <DrawerClose asChild onClick={() => setUserThread('')}>
-//                 <p className="cursor-pointer">Cancel</p>
-//             </DrawerClose>
-            
-//             <p className="font-medium">New Thread</p>
-            
-//             <button className="text-blue-500">Share</button>
-            
-//             </div>
-
-//             <div className="w-full h-[1px] bg-[#d7d7d7] dark:bg-[#464646] mb-2"/>
-
-//             <div className="flex p-6 items-start gap-3 w-full">
-
-//             <Image
-//                 src='/assets/images/user.png'
-//                 width={45}
-//                 height={45}
-//                 alt="profile-icon" 
-//             />
-
-//             <div className="flex flex-col w-full">
-
-//                 <p className="font-medium">Leslie Dsilva</p>
-                
-//                 <textarea placeholder="Start a thread..." rows={1} className="bg-white dark:bg-[#171717] outline-none resize-none overflow-hidden mb-2 placeholder:text-[#afafaf]  dark:placeholder:text-[#7a7a7a]" value={userThread} onChange={(e) => setUserThread(e.target.value)} ref={textAreaRef} required/>
-                
-//                 <Image
-//                     src='/assets/images/white-gallery.png'
-//                     width={18}
-//                     height={18}
-//                     alt="link-icon"
-//                     className="dark:hidden cursor-pointer"
-//                 />
-//                 <Image
-//                     src='/assets/images/black-gallery.png'
-//                     width={18}
-//                     height={18}
-//                     alt="link-icon"
-//                     className="hidden dark:block cursor-pointer"
-//                 />
-
-//             </div> 
-
-//             </div>
-
-//       </DrawerContent>
-//     </Drawer>
-//   )
-// }
 'use client'
 
 import Image from "next/image"
 import { useState , useEffect , useRef } from "react"
 import { UploadButton } from "@/utils/uploadthing"
-
-import { Button } from "@/components/ui/button"
-import {
-  Drawer,
-  DrawerClose,
-  DrawerContent,
-  DrawerDescription,
-  DrawerFooter,
-  DrawerHeader,
-  DrawerTitle,
-  DrawerTrigger,
-} from "@/components/ui/drawer"
-import { ArrowLeft, X } from "lucide-react"
+import { Drawer, DrawerClose, DrawerContent, DrawerTrigger } from "@/components/ui/drawer"
+import { X } from "lucide-react"
+import Loader from "./Loader"
+import Skeleton from "react-loading-skeleton"
 
 export function CreateDrawer() {
 
   const [userThread, setUserThread] = useState<string>('');
   const textAreaRef = useRef<HTMLTextAreaElement | null>(null);
   const [postImage,setPostImage] = useState<string>();
+  const [imageLoading,setImageLoading] = useState(false);
+  const [loading,setLoading] = useState(false);
+  const [openDrawer,setOpenDrawer] = useState(false);
 
   useEffect(() => {
     if (textAreaRef.current) {
       textAreaRef.current.style.height = 'auto';
       textAreaRef.current.style.height = textAreaRef.current.scrollHeight + 'px';
     }
-  },[userThread]);  
+  },[userThread]);
+  
+  function handleCancel(){
+    setUserThread('');
+    setPostImage('');
+  }
+
+  async function handleDone(){
+
+    setLoading(true);
+
+    setTimeout(()=>{                // Fake Delay
+        setLoading(false);
+        setOpenDrawer(false);
+    },3000);
+
+  }
 
   return (
     // dismissible = false is used to disable dragging of the drawer
-    <Drawer dismissible={false}>
+    <Drawer dismissible={false} open={openDrawer} onOpenChange={setOpenDrawer}>
 
       <DrawerTrigger asChild>
 
@@ -174,17 +70,17 @@ export function CreateDrawer() {
             
       </DrawerTrigger>
 
-      <DrawerContent className="w-full max-sm:h-[calc(100dvh)] sm:max-w-[575px] z-50" onInteractOutside={(e) => {setUserThread(''); setPostImage('')}}>
+      <DrawerContent className="w-full max-sm:h-[calc(100dvh)] sm:max-w-[575px] z-50">
 
-        <div className="flex justify-between p-6 px-8">
+        <div className="flex justify-between p-6 px-8 items-center">
   
-          <DrawerClose asChild onClick={() => {setUserThread(''); setPostImage('')}}>
-            <p className="cursor-pointer">Cancel</p>
+          <DrawerClose asChild onClick={handleCancel}>
+            <p className={`${imageLoading || loading ? 'cursor-pointer pointer-events-none h-[24px] w-[52px] flex justify-center' : 'cursor-pointer  w-[52px]  h-[24px] flex justify-center'}`}>{loading ? <Loader/> : 'Cancel'}</p>
           </DrawerClose>
   
           <p className="font-medium">New Thread</p>
   
-          <button className="text-blue-500">Share</button>
+          <button className={`${imageLoading ? 'text-blue-500  w-[52px] h-[24px] flex justify-center' : 'text-blue-500  w-[52px] h-[24px] flex justify-center'}`} onClick={handleDone}>{loading ? <Loader/> : 'Share'}</button>
 
         </div>
 
@@ -207,26 +103,42 @@ export function CreateDrawer() {
     
           <textarea placeholder="Start a thread..." rows={1} className="bg-white dark:bg-[#171717] outline-none resize-none overflow-hidden mb-2 placeholder:text-[#afafaf]  dark:placeholder:text-[#7a7a7a]" value={userThread} onChange={(e) => setUserThread(e.target.value)} ref={textAreaRef} required/>
 
-          { postImage ? 
- 
-            <div className="w-fit relative border border-[#d1d1d1] rounded-xl">
-              <img
-                src={postImage}
-                alt="post-image"
-                className="h-[300px] w-full rounded-xl"
-              />
-              <div className="absolute top-4 right-3 p-1 cursor-pointer bg-[#6b6b6b] bg-opacity-60 rounded-full" onClick={() => setPostImage('')}>
-                <X width={18} height={18} className="text-white"/>
-              </div>
-            </div> 
+          { imageLoading || postImage ? 
+
+              ( postImage ?
+
+                <div className="w-fit relative border border-[#d1d1d1] dark:!border-[#393939] rounded-xl">
+                  
+                  <img
+                    src={postImage}
+                    alt="post-image"
+                    className="h-[300px] w-full rounded-xl"
+                  />
+                  
+                  <div className="absolute top-4 right-3 p-1 cursor-pointer bg-[#6b6b6b] bg-opacity-60 rounded-full" onClick={() => setPostImage('')}>
+                    <X width={18} height={18} className="text-white"/>
+                  </div>
+                
+                </div> 
+
+                :
+
+                <Skeleton width={300} height={300}/>
+
+              )
     
             : 
 
             <UploadButton endpoint='imageUploader' className="max-w-fit"
 
+              onUploadProgress={()=>{
+                setImageLoading(true);
+              }}
+
               onClientUploadComplete={(res) => {
                 // Do something with the response
                 console.log("Files: ", res);
+                setImageLoading(false);
                 setPostImage(res[0].url);
               }}
               onUploadError={(error: Error) => {
