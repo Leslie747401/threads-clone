c75720c1a45224274336813b70876ebdfafbdc57
Leslie747401 <leslie747401@gmail.com>
2024-03-17 01:01:04 +0530

    Fixed some deployment errors - 1

--- components/Room.tsx
+++ components/Room.tsx
@@ -1,12 +1,12 @@
-"use client";
+// "use client";
 
-import { ReactNode } from "react";
-import { RoomProvider } from '@/liveblocks.config';
+// import { ReactNode } from "react";
+// // import { RoomProvider } from '@/liveblocks.config';
 
-export function Room({ children }: { children: ReactNode }) {
-  return (
-    <RoomProvider id="my-room" initialPresence={{}}>
-        {children}
-    </RoomProvider>
-  );
-}
+// export function Room({ children }: { children: ReactNode }) {
+//   return (
+//     <RoomProvider id="my-room" initialPresence={{}}>
+//         {children}
+//     </RoomProvider>
+//   );
+// }
